5f6f3213d8e9204653b5e59f3ecc2eaf9117b470
infraarhtur <infraarhtur@gmail.com>
2025-07-17 19:45:33 -0500

    -lm:creacion de segundo reporte ventas por producto

--- app/data/report_repository.py
+++ app/data/report_repository.py
@@ -6,6 +6,7 @@ from sqlalchemy import text
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import SQLAlchemyError
 
+from app.models.reports.report_sales_by_products_model import ReportSalesByProductsModel
 from app.models.reports.report_summary_sales_model import SalesSummaryByPaymentModel
 
 
@@ -32,4 +33,27 @@ class ReportRepository:
             ]
         except SQLAlchemyError as e:
             print("❌ Error al ejecutar el resumen de ventas:", e)
+            raise e
+
+    def get_sales_by_products(self, start_date: date, end_date: date) -> List[ReportSalesByProductsModel]:
+        try:
+            query = text("""
+                SELECT * FROM public.get_sales_by_products(:start_date, :end_date)
+            """)
+            result = self.db.execute(query, {"start_date": start_date, "end_date": end_date})
+            rows = result.fetchall()
+
+            return [
+                ReportSalesByProductsModel(
+                    product_name=row[0],
+                    purchase_price=row[1],
+                    sale_price=row[2],
+                    total_units_sold=row[3],
+                    total_revenue=row[4],
+                    total_discount=row[5]
+                )
+                for row in rows
+            ]
+        except SQLAlchemyError as e:
+            print("❌ Error al ejecutar el reporte de ventas por productos:", e)
             raise e
